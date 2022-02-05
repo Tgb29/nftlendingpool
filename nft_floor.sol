2044b155f7908664ba9b58c95569ba0c73275643
theodorebrinkofski <tgb29@cornell.edu>
2022-02-05 16:27:31 -0500

    first commit. Testnet contract that works

---
+++ nft_floor.sol
@@ -0,0 +1,71 @@
+pragma solidity ^0.6.0;
+
+import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
+
+contract APIConsumer is ChainlinkClient {
+  
+    uint256 public price;
+    
+    address private oracle;
+    bytes32 private jobId;
+    uint256 private fee;
+    
+    /**
+     * Network: Polygon Mumbai Testnet
+     * Oracle: 0x58bbdbfb6fca3129b91f0dbe372098123b38b5e9
+     * Job ID: da20aae0e4c843f6949e5cb3f7cfe8c4
+     * LINK address: 0x326C977E6efc84E512bB9C30f76E30c160eD06FB
+     * Fee: 0.01 LINK
+     */
+    constructor() public {
+        setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
+        oracle = 0x58bbdbfb6fca3129b91f0dbe372098123b38b5e9;
+        jobId = "da20aae0e4c843f6949e5cb3f7cfe8c4";
+        fee = 10 ** 16; // 0.01 LINK
+    }
+    
+    /**
+     * Create a Chainlink request to retrieve API response, find the target price
+     * data, then multiply by 100 (to remove decimal places from price).
+     */
+    function requestPrice() public returns (bytes32 requestId) 
+    {
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        
+        request.add("get", "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=EUR");
+        
+       // Set the path to find the desired data in the API response, where the response format is:
+       // {
+       //     "Realtime Currency Exchange Rate": {
+       //       "1. From_Currency Code": "BTC",
+       //       "2. From_Currency Name": "Bitcoin",
+       //       "3. To_Currency Code": "CNY",
+       //       "4. To_Currency Name": "Chinese Yuan",
+       //       "5. Exchange Rate": "207838.88814500",
+       //       "6. Last Refreshed": "2021-01-26 11:11:07",
+       //       "7. Time Zone": "UTC",
+       //      "8. Bid Price": "207838.82343000",
+       //       "9. Ask Price": "207838.88814500"
+       //     }
+       //     }
+        //string[] memory path = new string[](2);
+        //path[0] = "Realtime Currency Exchange Rate";
+        //path[1] = "5. Exchange Rate";
+        //request.addStringArray("path", path);
+        request.add("path", "EUR");
+        
+        // Multiply the result by 10000000000 to remove decimals
+        request.addInt("times", 100);
+        
+        // Sends the request
+        return sendChainlinkRequestTo(oracle, request, fee);
+    }
+    
+    /**
+     * Receive the response in the form of uint256
+     */ 
+    function fulfill(bytes32 _requestId, uint256 _price) public recordChainlinkFulfillment(_requestId)
+    {
+        price = _price;
+    }
+}
